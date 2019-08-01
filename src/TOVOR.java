5a25326fdcbd4756728f272231e2de78d7cabdbb
Torres Fernendez <carlos-alfonso.torres-fernendez@dxc.com>
2019-08-01 17:58:26 -0500

    First random attempt

--- src/TOVOR.java
+++ src/TOVOR.java
@@ -1,9 +1,14 @@
+import java.util.Random;
+
 import aiinterface.AIInterface;
 import struct.FrameData;
 import struct.GameData;
 import struct.Key;
 
 public class TOVOR implements AIInterface {
+	
+	Key key;
+	Random rand;
 
 	@Override
 	public void close() {
@@ -20,19 +25,27 @@ public class TOVOR implements AIInterface {
 	@Override
 	public int initialize(GameData arg0, boolean arg1) {
 		// TODO Auto-generated method stub
+		rand = new Random();
+		key = new Key();
 		return 0;
 	}
 
 	@Override
 	public Key input() {
 		// TODO Auto-generated method stub
-		return null;
+		return key;
 	}
 
 	@Override
 	public void processing() {
 		// TODO Auto-generated method stub
-
+		key.A = (rand.nextInt(100) > 70);
+		key.B = (rand.nextInt(100) > 70);
+		key.C = (rand.nextInt(100) > 70);
+		key.D = (rand.nextInt(100) > 70);
+		key.U = (rand.nextInt(100) > 70);
+		key.L = (rand.nextInt(100) > 70);
+		key.R = (rand.nextInt(100) > 70);
 	}
 
 	@Override
