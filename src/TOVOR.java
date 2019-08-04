bb2737e19c92411d9cdd7bc25d3f86dff5e64739
Carlos Torres <carlos.torres@gameover.com.mx>
2019-08-03 21:45:35 -0500

    Implemented highest ranked moves

--- src/TOVOR.java
+++ src/TOVOR.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import aiinterface.AIInterface;
 import aiinterface.CommandCenter;
 import struct.FrameData;
 import struct.GameData;
 import struct.Key;
 import enumerate.Action;
+import simulator.Simulator;
 
 public class TOVOR implements AIInterface {
 	
@@ -11,6 +15,7 @@ public class TOVOR implements AIInterface {
 	CommandCenter cc;
 	boolean cPlayer;
 	FrameData fd;
+	GameData gd;
 
 	@Override
 	public void close() {
@@ -32,6 +37,7 @@ public class TOVOR implements AIInterface {
 		cc = new CommandCenter();
 		cPlayer = arg1;
 		fd = new FrameData();
+		gd = arg0;
 		return 0;
 	}
 
@@ -46,20 +52,44 @@ public class TOVOR implements AIInterface {
 		// TODO Auto-generated method stub
 		if(!fd.getEmptyFlag() && fd.getRemainingFramesNumber()>0) 
 		{
-				System.out.println(fd.getCharacter(cPlayer).getAction());
-				System.out.println(fd.getDistanceX());
+			Action action = fd.getCharacter(cPlayer).getAction();
+			if(fd.getCharacter(cPlayer).getEnergy() > 150 && action == Action.STAND) 
+			{
+				if (fd.getDistanceX() > 300)
+				{
+					cc.commandCall(Action.STAND_D_DF_FC.toString());
+				}
+				else
+				{
+					cc.commandCall(Action.BACK_JUMP.toString());
+				}
+			}
+			else 
+			{
 				if (fd.getDistanceX() > 100)
 				{
 					cc.commandCall("6");
 				}
 				else
 				{
-					if(fd.getCharacter(cPlayer).getAction() == Action.STAND)
+					if(action == Action.STAND)
 					{
-						cc.commandCall("A");
+						LinkedList<Action> myact = new LinkedList<Action>(Arrays.asList(Action.STAND_FB));
+						LinkedList<Action> opact =  new LinkedList<Action>(Arrays.asList(Action.STAND));
+						Simulator sim = new Simulator(gd);
+						FrameData sd = sim.simulate(fd, cPlayer, myact, opact, 14);
+						if(sd.getCharacter(cPlayer).isHitConfirm())
+						{
+							cc.commandCall(Action.STAND_FB.toString());
+						}
+						else
+						{
+							cc.commandCall("B");
+						}
 					}
 				}
-				key = cc.getSkillKey();
+			}
+			key = cc.getSkillKey();
 		}
 
 	}
